f72ffaacae13125a28ed2b5281c6d921bf9a8054
Renan Marcel <renan@wearepropeople.com.br>
2014-08-21 22:55:18 -0300

    Second commit

--- src/imagestudio/ImageStudio.java
+++ src/imagestudio/ImageStudio.java
@@ -7,6 +7,9 @@
 package imagestudio;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author renanmfd
@@ -21,5 +24,20 @@ public class ImageStudio {
         File f = new File("images/img0.png");
         
     }
+    
+    public int getObject(Object obj) {
+    for (Field field : obj.getClass().getDeclaredFields()) {
+        try {
+            //field.setAccessible(true); // if you want to modify private fields
+            System.out.println(field.getName()
+                    + " - " + field.getType()
+                    + " - " + field.get(obj));
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(ImageStudio.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(ImageStudio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
     
 }
