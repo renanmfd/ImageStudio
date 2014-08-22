672c4bb9eec42efa06c2491bfb603ae07d4abef1
Renan Marcel <renan@wearepropeople.com.br>
2014-08-21 22:30:31 -0300

    First commit / Project start

---
+++ src/imagestudio/ImageStudio.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package imagestudio;
+
+import java.io.File;
+
+/**
+ * @author renanmfd
+ */
+public class ImageStudio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        File f = new File("images/img0.png");
+        
+    }
+    
+}
